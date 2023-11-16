22d1185c95b1e4d7eeb4d814685309ea57d27448
romys <romysaputrasihanandaa@gmail.com>
2023-11-16 11:25:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import speedtest from "./utils/speed";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      speed: null,
+    };
+  }
+
+  handleSpeed = async () => {
+    const speed = await speedtest();
+    console.log(speed);
+    this.setState({ speed });
+  };
+
+  render = () => {
+    const { speed } = this.state;
+
+    return (
+      <div>
+        <h1>Speed Test</h1>
+
+        <button onClick={this.handleSpeed}>Test dong</button>
+
+        {speed && (
+          <div>
+            <p>download : {speed.download}</p>
+            <p>upload : {speed.upload}</p>
+          </div>
+        )}
+      </div>
+    );
+  };
+}
+
+export default App;
---
+++ src/utils/speed.js
@@ -0,0 +1,58 @@
+const runSpeedTest = async () => {
+  const fileSizeInMB = 10;
+  const fileUrl = "https://pbs.twimg.com/media/F7u3iB-boAAzYT9.jpg";
+
+  const startTime = new Date().getTime();
+
+  try {
+    const response = await fetch(fileUrl);
+    const data = await response.arrayBuffer();
+    const endTime = new Date().getTime();
+    const downloadSpeed = parseFloat(
+      (fileSizeInMB / ((endTime - startTime) / 1000)).toFixed(2)
+    );
+
+    const uploadStartTime = new Date().getTime();
+    const formData = new FormData();
+    formData.append("file", new Blob([data]));
+
+    await fetch(fileUrl, { method: "POST", body: formData });
+
+    const uploadEndTime = new Date().getTime();
+    const uploadSpeed = parseFloat(
+      (fileSizeInMB / ((uploadEndTime - uploadStartTime) / 1000)).toFixed(2)
+    );
+
+    return {
+      downloadSpeed,
+      uploadSpeed,
+    };
+  } catch (error) {
+    throw new Error("Error dalam speed test: " + error);
+  }
+};
+
+const speedtest = async () => {
+  try {
+    let upload = 0;
+    let download = 0;
+    // for (let i = 0; i < 3; i++) {
+    const { downloadSpeed, uploadSpeed } = await runSpeedTest();
+    //   download += downloadSpeed;
+    //   upload += uploadSpeed;
+    // }
+    // return {
+    //   updoad: parseFloat((upload / 3).toFixed(2)),
+    //   download: parseFloat((download / 3).toFixed(2)),
+    // };
+
+    return {
+      updoad: uploadSpeed,
+      download: downloadSpeed,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default speedtest;
